7ad2c174b8c0786858bfce09513050439a3c9b76
nayan0499 <nayangrg.uni@gmail.com>
2023-10-10 11:58:17 +0100

    Make login button functional

--- MobileAcebook/LogInView.swift
+++ MobileAcebook/LogInView.swift
@@ -9,13 +9,20 @@ import Foundation
 import SwiftUI
 import Combine
 
+
 struct LogInView: View {
     @State private var userModel = User(email: "", password: "")
     
     private func onEmailInputChanged(changedEmail: String) {
-            print("-----> in onEmailInputChanged: \(changedEmail) ")
+        userModel.email = changedEmail
+        print("changed email: \(userModel.email)")
         }
     
+    private func onPasswordInputChanged(changedPassword: String) {
+        userModel.password = changedPassword
+        print("changed password: \(userModel.password)")
+        }
+    let authenticationService = AuthenticationService()
     var body: some View {
         
         ZStack {
@@ -30,17 +37,25 @@ struct LogInView: View {
                     .accessibilityIdentifier("makers-logo")
                 
                 Spacer()
+                LabeledContent {
+                  TextField("Email", text: $userModel.email)
+                } label: {
+                  Text("Email")
+                }.onChange(of: userModel.email, perform: onEmailInputChanged)
                 
-                TextField("Email", text: $userModel.email)
-                     .onChange(of: userModel.email, perform: onEmailInputChanged)
+                LabeledContent {
+                  TextField("Password", text: $userModel.password)
+                } label: {
+                  Text("Password")
+                }.onChange(of: userModel.password, perform: onPasswordInputChanged)
                 
-                NavigationLink(destination: LogInView()) {
-                    Text("Login")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(Color.white)
-                        .cornerRadius(10)
+                
+                Button(action: {authenticationService.login(user: userModel)}) {
+                    // TODO: login logic
+                    Text("login")
+                   
                 }
+                .accessibilityIdentifier("LoginButton")
                 
                 Button("Sign Up") {
                     // TODO: sign up logic
