ae8bc9ca6721c03683588a036242ca4cee51eb15
nayan0499 <nayangrg.uni@gmail.com>
2023-10-10 11:59:19 +0100

    Implement login function

--- MobileAcebook/Services/AuthenticationService.swift
+++ MobileAcebook/Services/AuthenticationService.swift
@@ -4,10 +4,44 @@
 //
 //  Created by Josué Estévez Fernández on 01/10/2023.
 //
+import Foundation
 
 class AuthenticationService: AuthenticationServiceProtocol {
     func signUp(user: User) -> Bool {
         // Logic to call the backend API for signing up
         return true // placeholder
     }
+    func login(user: User) -> String {
+        
+        var request = URLRequest(url: URL(string: "http://localhost:8080/tokens")!)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                // Handle response here
+                if let error = error {
+                    print("Error: \(error)")
+                } else if let data = data {
+                    do {
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                        print("Response: \(jsonResponse)")
+                    } catch {
+                        print("Error parsing JSON: \(error)")
+                    }
+                }
+            }
+            task.resume()
+        } catch {
+            print("Error encoding user object: \(error)")
+        }
+        return "test"
+//
+    }
+    
+    
 }
