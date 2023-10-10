2a5ae64d06965f84c3c7431b421e0b5fe144b3bc
Jennifer Wark <128541733+jenny4567@users.noreply.github.com>
2023-10-10 11:47:35 +0100

    post method to make new user works, messy otherwise

--- MobileAcebook/WelcomePageView.swift
+++ MobileAcebook/WelcomePageView.swift
@@ -8,18 +8,20 @@
 import SwiftUI
 
 struct WelcomePageView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
     var body: some View {
         ZStack {
             VStack {
                 Spacer()
-
+                
                 Text("Welcome to Acebook!")
                     .font(.largeTitle)
                     .padding(.bottom, 20)
                     .accessibilityIdentifier("welcomeText")
-
+                
                 Spacer()
-
+                
                 Image("makers-logo")
                     .resizable()
                     .scaledToFit()
@@ -27,13 +29,32 @@ struct WelcomePageView: View {
                     .accessibilityIdentifier("makers-logo")
                 
                 Spacer()
-
+                
+                
+                Form {
+                    TextField("email", text: $email)
+                    TextField("password", text: $password)
+                }
+                
+                Spacer()
+                
                 Button("Sign Up") {
-                    // TODO: sign up logic
+                    var request = URLRequest(url: URL(string: "http://127.0.0.1:8080/users")!)
+                    request.httpMethod = "POST"
+                    let json: [String: Any] = ["email": email, "password": password]
+                    let jsonData = try? JSONSerialization.data(withJSONObject: json)
+                    request.httpBody = jsonData
+                    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                        if let error = error {
+                            print("Error: \(error.localizedDescription)")}
+                    }
+                    task.resume()
                 }
                 .accessibilityIdentifier("signUpButton")
                 
                 Spacer()
+                
             }
         }
     }
