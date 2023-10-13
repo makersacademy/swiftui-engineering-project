4692a275cbd6dbc52c18bf1a44a4919f1bcd563f
Banji Ajayi <banji.ajayi@holidayextras.com>
2023-10-13 10:41:47 +0100

    added design to welcome view

--- MobileAcebook/WelcomePageView.swift
+++ MobileAcebook/WelcomePageView.swift
@@ -9,22 +9,43 @@ struct WelcomePageView: View {
  var body: some View {
   NavigationView {
    ZStack {
+       Color.blue
+            .ignoresSafeArea()
+        Circle()
+            .scale(1.7)
+            .foregroundColor(.white.opacity(0.15))
+        Circle()
+            .scale(1.35)
+            .foregroundColor(.white)
     VStack {
      Spacer()
      Text("Welcome to Acebook!")
       .font(.largeTitle)
       .padding(.bottom, 20)
       .accessibilityIdentifier("welcomeText")
-     Spacer()
+//     Spacer()
      Image("makers-logo")
       .resizable()
       .scaledToFit()
       .frame(width: 200, height: 200)
       .accessibilityIdentifier("makers-logo")
+        
+        
+    
+    NavigationLink("Log In", destination: LogInView())
+        .accessibilityIdentifier("logInButton")
+        .foregroundColor(.white)
+        .frame(width: 250, height: 50)
+        .background(Color.blue.opacity(0.80))
+        .cornerRadius(10)
+        
      NavigationLink("Sign Up", destination: SignUpView())
      .accessibilityIdentifier("signUpButton")
-      NavigationLink("Log In", destination: LogInView())
-      .accessibilityIdentifier("logInButton")
+//     .foregroundColor(.white)
+//     .frame(width: 300, height: 50)
+//     .background(Color.blue)
+//     .cornerRadius(10)
+
      Spacer()
     }
    }
