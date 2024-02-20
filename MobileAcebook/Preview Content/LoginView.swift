1952034dc358e3af2640a25722c49118914693b7
Gabriela <gabrielaehrenbrink@gmail.com>
2024-02-20 12:26:22 +0000

    first commit

--- MobileAcebook/Preview Content/LoginView.swift
+++ MobileAcebook/Preview Content/LoginView.swift
@@ -6,5 +6,77 @@
 //
 
 import Foundation
+import SwiftUI
 
-
+struct LoginPage: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var wrongEmail = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    var authenticationService: AuthenticationServiceProtocol
+    
+    var body: some View {
+        NavigationView {
+            ZStack{
+                Color(red: Double(67) / 255.0,
+                      green: Double(100) / 255.0,
+                      blue: Double(157) / 255.0
+                )
+                .ignoresSafeArea()
+                VStack {
+                    Text("Acebook")
+                        .font(.system(size: 70))
+                        .bold()
+                        .padding()
+                        .foregroundColor(Color.white)
+                    TextField("Email", text: $email)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongEmail))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                        authenticateUser(email: email, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 100, height: 50)
+                    .background(Color(red: Double((0x254778 & 0xFF0000) >> 16) / 255.0, green: Double((0x254778 & 0x00FF00) >> 8) / 255.0, blue: Double(0x254778 & 0x0000FF) / 255.0))
+                    .cornerRadius(10)
+                    .bold()
+                    
+                    
+                }
+                
+            }
+            .navigationBarHidden(true)
+        }
+        
+    }
+    func authenticateUser(email: String, password: String) {
+        authenticationService.logIn(email: email, password: password) { success in
+            if success {
+                // Authentication succeeded, you can navigate to the next screen or perform other actions
+                print("Authentication successful!")
+            } else {
+                // Authentication failed, update UI accordingly (e.g., show an error message)
+                print("Authentication failed!")
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            LoginPage(authenticationService: AuthenticationService())
+        }
+    }
+    
+}
--- MobileAcebook/Services/AuthenticationService.swift
+++ MobileAcebook/Services/AuthenticationService.swift
@@ -39,4 +39,36 @@ class AuthenticationService: ObservableObject, AuthenticationServiceProtocol {
             completion(false, error)
         }
     }
+    func logIn(user: User, completion: @escaping (Bool, Error?) -> Void) {
+        guard let url = URL(string: "http://127.0.0.1:8080/")?.appendingPathComponent("users") else {
+            completion(false, nil)
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            let encoder = JSONEncoder()
+            let userData = try encoder.encode(user)
+            request.httpBody = userData
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    completion(false, error)
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                    completion(false, nil)
+                    return
+                }
+                
+                completion(true, nil)
+            }.resume()
+        } catch {
+            completion(false, error)
+        }
+    }
 }
