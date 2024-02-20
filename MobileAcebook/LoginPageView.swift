0ff7344164d65ef03b82e807879604dd192a0664
Holly <holly.page@imshi.co.uk>
2024-02-20 14:45:21 +0000

    Login check email is in database and password is correct, redirects to new page if these details are true

--- MobileAcebook/LoginPageView.swift
+++ MobileAcebook/LoginPageView.swift
@@ -11,68 +11,109 @@ struct LoginPageView: View {
     
     @State private var email = ""
     @State private var password = ""
+    @State private var isAuthenticated = false
     
     var body: some View {
-        ZStack {
-            VStack {
-                Spacer()
-
-                Image("makers-logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .accessibilityIdentifier("makers-logo")
-
-                Text("Please enter your details")
-                    .font(.caption)
-                    .bold()
-                   
-                Spacer()
-                
-                VStack(alignment: .leading, spacing: 15) {
-                
+        NavigationView {
+            ZStack {
+                VStack {
+                    Spacer()
                     
-                    TextField("", text: self.$email, prompt: Text("Email").foregroundColor(.white))
-                        .padding()
-                        .background(.black)
-                        .cornerRadius(20.0)
-                        .foregroundColor(.white)
+                    Image("makers-logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                        .accessibilityIdentifier("makers-logo")
+                    
+                    Text("Please enter your details")
+                        .font(.caption)
+                        .bold()
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .leading, spacing: 15) {
+                        TextField("", text: self.$email, prompt: Text("Email").foregroundColor(.white))
+                            .padding()
+                            .background(.black)
+                            .cornerRadius(20.0)
+                            .foregroundColor(.white)
                         
-                    SecureField("", text: self.$password, prompt: Text("Password").foregroundColor(.white))
-                        .padding()
-                        .background(.black)
-                        .cornerRadius(20.0)
-                        .foregroundColor(.white)
+                        SecureField("", text: self.$password, prompt: Text("Password").foregroundColor(.white))
+                            .padding()
+                            .background(.black)
+                            .cornerRadius(20.0)
+                            .foregroundColor(.white)
+                    }.padding([.leading, .trailing], 27.5)
                     
-                }.padding([.leading, .trailing], 27.5)
-                
-                Spacer()
-                
-                Button("Login") {
-                    // TODO: log in logic
-                }.font(.headline)
-                .foregroundColor(.white)
-                .frame(width: 300, height: 50)
-                .background(Color.black)
-                .cornerRadius(15.0)
-                .accessibilityIdentifier("loginButton")
-                
-                Spacer()
-                Divider()
-            
-                VStack{
-                    Text("Don't have an account?").bold()
-                    Button("Click here"){
-                        //Redirects to sign up
-                    }.foregroundColor(.white)
+                    Spacer()
+                    
+                    Button("Login") {
+                        login()
+                    }.font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.black)
+                    .cornerRadius(15.0)
+                    .accessibilityIdentifier("loginButton")
+                    
+                    Spacer()
+                    Divider()
+                    
+                    VStack{
+                        Text("Don't have an account?").bold()
+                        
+                        Button("Click here"){
+                            //Redirects to sign up
+                        }.foregroundColor(.white)
                         .frame(width: 100, height: 30)
                         .background(Color.black)
                         .cornerRadius(15.0)
                         .accessibilityIdentifier("clickHereButton")
+                    }
                 }
-                    
             }
         }
+        .navigationBarHidden(true)
+        .fullScreenCover(isPresented: $isAuthenticated) {
+            // This will present the PostPageView when isAuthenticated becomes true
+            //PostPageView()
+        }
+    }
+    
+    private func login() {
+        guard let url = URL(string: "http://127.0.0.1:8080/tokens") else {
+            print("Invalid URL")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let parameters = ["email": email, "password": password]
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters) else {
+            print("Invalid parameters")
+            return
+        }
+        
+        request.httpBody = httpBody
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data, let token = String(data: data, encoding: .utf8) {
+                if isValidToken(token) {
+                    print("Authentication token: \(token)")
+                    isAuthenticated = true
+                } else {
+                    isAuthenticated = false
+                    print("Invalid token: Authentication failed")
+                }
+            }
+        }.resume()
+    }
+    
+    private func isValidToken(_ token: String) -> Bool {
+        
+        return token.count > 50
     }
 }
 
