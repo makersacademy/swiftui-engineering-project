af8ce66492b6b7b70caa02d88a462993abddbe06
aisha <aisha_mo@hotmail.com>
2024-02-20 11:57:22 +0000

    signup function works - not tested

---
+++ MobileAcebook/Models/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  MobileAcebook
+//
+//  Created by Aisha Mohamed on 19/02/2024.
+//
+
+import Foundation
+
+public struct Post: Hashable, Codable {
+    let message: String
+    let image: String    
+}
