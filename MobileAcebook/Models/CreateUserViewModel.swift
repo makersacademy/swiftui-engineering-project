370d4c43e5c1c48ef3c06bd45b83f189a122029a
Josh Bhogal <joshuabhogal@outlook.com>
2024-02-20 15:14:49 +0000

    Signup page integration

---
+++ MobileAcebook/Models/CreateUserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CreateUserViewModel.swift
+//  MobileAcebook
+//
+//  Created by Joshua Bhogal on 20/02/2024.
+//
+
+import Foundation
---
+++ MobileAcebook/SignupPageView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupPageView.swift
+//  MobileAcebook
+//
+//  Created by Joshua Bhogal on 20/02/2024.
+//
+
+import SwiftUI
+
+struct SignupPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupPageView()
+}
