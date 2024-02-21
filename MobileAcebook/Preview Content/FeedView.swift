e887c15c53678f7c8ad34790f5457ddbaaed2305
aisha <aisha_mo@hotmail.com>
2024-02-21 19:11:45 +0000

    lower navbar added, active page is filled and blue

--- MobileAcebook/Preview Content/FeedView.swift
+++ MobileAcebook/Preview Content/FeedView.swift
@@ -11,43 +11,100 @@ struct FeedView: View {
 //    var user: User?
     @State private var isLoggedOut = false
     @State private var isPersonClicked = false
+    @State private var isHouseClicked = false
     var body: some View {
         
         NavigationView {
-            List {
-                Text("Hello, SwiftUI!")
-                Text("Hello, SwiftUI!")
-                Text("Hello, SwiftUI!")
-                Text("Hello, SwiftUI!")
-                Text("Hello, SwiftUI!")
-                Text("Hello, SwiftUI!")
+            VStack{
+                List {
+                    Text("Post 1")
+                        .padding()
+                    Text("Post 2")
+                        .padding()
+                    Text("Post 3")
+                        .padding()
+                    Text("Post 4")
+                        .padding()
+                    Text("Post 5")
+                        .padding()
+                    Text("Post 6")
+                        .padding()
+                    Text("Post 7")
+                        .padding()
+                    Text("Post 8")
+                        .padding()
+                    Text("Post 9")
+                        .padding()
+                    Text("Post 10") .padding()
+                    Text("Post 11") .padding()
+                    Text("Post 12") .padding()
+                    Text("Post 13") .padding()
+                    Text("Post 14") .padding()
+                    Text("Post 15") .padding()
+                    Text("Post 16") .padding()
+                    Text("Post 17") .padding()
+                    Text("Post 18") .padding()
+                    Text("Post 19") .padding()
+                    Text("Post 20") .padding()
+                    Text("Post 21") .padding()
+                }
             }
             .navigationTitle("Feed")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: ProfileView(), isActive: $isPersonClicked) {
                         Button(action: {
-                            // Action to perform for navigating to ProfileView
-                            isPersonClicked = true
+                            isLoggedOut = true
                         }) {
-                            Image(systemName: isPersonClicked ? "person.fill" : "person")
-                                .foregroundColor(.black)
+                            Image(systemName: isLoggedOut ? "sun.max.fill" : "sun.max")
+                                .foregroundColor(Color(UIColor(rgb: 0x1877F2)))
+                        }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: LoginPage(authenticationService: AuthenticationService()), isActive: $isLoggedOut) {
+                        Button(action: {
+                            isLoggedOut = true
+                        }) {
+                            Image(systemName: isLoggedOut ? "rectangle.portrait.and.arrow.right.fill" : "rectangle.portrait.and.arrow.right")
+                                .foregroundColor(Color(UIColor(rgb: 0x1877F2)))
                         }
                     }
                 }
                 
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: LoginPage(authenticationService: AuthenticationService()), isActive: $isLoggedOut) {
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    NavigationLink(destination: FeedView(), isActive: $isHouseClicked) {
                         Button(action: {
-                            // Action to perform for logout
-                            isLoggedOut = true
+                            isHouseClicked = true
                         }) {
-                            Image(systemName: isLoggedOut ? "rectangle.portrait.and.arrow.right.fill" : "rectangle.portrait.and.arrow.right")
-                                .foregroundColor(.black)
+                            Image(systemName: "house.fill")
+                                .foregroundColor(Color(UIColor(rgb: 0x316ff6)))
                         }
                     }
+                    Spacer()
+                    Image(systemName: "play.tv")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    Image(systemName: "person.2")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    NavigationLink(destination: ProfileView(), isActive: $isPersonClicked) {
+                        Button(action: {
+                            isPersonClicked = true
+                        }) {
+                            Image(systemName: "person.crop.circle")
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                    Spacer()
+                    Image(systemName: "bell")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    Image(systemName: "line.3.horizontal")
+                        .foregroundColor(Color.gray)
                 }
             }
+
         }
         .navigationBarBackButtonHidden(true)
         .navigationBarHidden(true)
--- MobileAcebook/Preview Content/ProfileView.swift
+++ MobileAcebook/Preview Content/ProfileView.swift
@@ -6,6 +6,7 @@ import SwiftUI
 
 struct ProfileView: View {
     @State private var isLoggedOut = false
+    @State private var isPersonClicked = false
     @State private var isHouseClicked = false
     
     var body: some View {
@@ -18,26 +19,56 @@ struct ProfileView: View {
             .navigationTitle("Profile")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: FeedView(), isActive: $isHouseClicked) {
                         Button(action: {
-                            // Action to perform for navigating to ProfileView
-                            isHouseClicked = true
+                            isLoggedOut = true
                         }) {
-                            Image(systemName: isHouseClicked ? "house.fill" : "house")
-                                .foregroundColor(.black)
+                            Image(systemName: isLoggedOut ? "sun.max.fill" : "sun.max")
+                                .foregroundColor(Color(UIColor(rgb: 0x1877F2)))
                         }
-                    }
                 }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     NavigationLink(destination: LoginPage(authenticationService: AuthenticationService()), isActive: $isLoggedOut) {
                         Button(action: {
-                            // Action to perform for logout
+
                             isLoggedOut = true
                         }) {
                             Image(systemName: isLoggedOut ? "rectangle.portrait.and.arrow.right.fill" : "rectangle.portrait.and.arrow.right")
-                                .foregroundColor(.black)
+                                .foregroundColor(Color(UIColor(rgb: 0x316ff6)))
                         }
                     }
+                }
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    NavigationLink(destination: FeedView(), isActive: $isHouseClicked) {
+                        Button(action: {
+                            isHouseClicked = true
+                        }) {
+                            Image(systemName: "house")
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                    Spacer()
+                    Image(systemName: "play.tv")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    Image(systemName: "person.2")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    NavigationLink(destination: ProfileView(), isActive: $isPersonClicked) {
+                        Button(action: {
+                            isPersonClicked = true
+                        }) {
+                            Image(systemName: isPersonClicked ? "person.crop.circle" : "person.crop.circle.fill")
+                                .foregroundColor(Color(UIColor(rgb: 0x316ff6)))
+                        }
+                    }
+                    Spacer()
+                    Image(systemName: "bell")
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                    Image(systemName: "line.3.horizontal")
+                        .foregroundColor(Color.gray)
                 }
             }
             .background(
