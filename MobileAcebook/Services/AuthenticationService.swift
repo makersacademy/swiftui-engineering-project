f1c7a86d0f7d34a2c3063fc68dc1cddc73310f6b
Maria Jose <mj_hernandezdelprado@hotmail.com>
2024-04-16 12:49:45 +0100

    Database connection

--- MobileAcebook/Services/AuthenticationService.swift
+++ MobileAcebook/Services/AuthenticationService.swift
@@ -1,31 +1,44 @@
-//
-//  AuthenticationService.swift
-//  MobileAcebook
-//
-//  Created by Josué Estévez Fernández on 01/10/2023.
-//
+
 import Foundation
-class AuthenticationService {
-    func signUp(user: User, completion: @escaping (Bool) -> Void) {
-        let url = URL(string: "http://localhost:3000/users")! // Replace with your backend URL
+
+protocol AuthenticationServiceProtocol {
+    func signUp(user: User, completion: @escaping (Bool, String?) -> Void)
+}
+
+struct User: Codable {
+    let username: String
+    let password: String
+    let email: String
+    let imgUrl: String
+}
+
+class AuthenticationService: AuthenticationServiceProtocol {
+    func signUp(user: User, completion: @escaping (Bool, String?) -> Void) {
+        let url = URL(string: "http://localhost:3000/users")! // Use http instead of https if you're not setup for SSL on local
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
-        let encoder = JSONEncoder()
         do {
-            let userData = try encoder.encode(user)
-            request.httpBody = userData
-
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                if let _ = data, let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 201 {
-                    completion(true) // Signup successful
-                } else {
-                    completion(false) // Signup failed
-                }
-            }.resume()
+            request.httpBody = try JSONEncoder().encode(user)
         } catch {
-            completion(false) // Encoding error
+            completion(false, "Failed to encode user data")
+            return
         }
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 201 else {
+                completion(false, "Failed to register user")
+                return
+            }
+            
+            if let error = error {
+                completion(false, "Request error: \(error.localizedDescription)")
+                return
+            }
+            
+            completion(true, nil)
+        }
+        task.resume()
     }
 }
